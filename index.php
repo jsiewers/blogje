a9123325cae43d3d7e641e707dcc2eba51af45a7
janjaap <janjaap@roc-dev.com>
2016-07-13 12:40:58 +0200

    Database test

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php 
-$user = 'root';
-$password = 'root';
-$db = 'blogje';
-$host = 'localhost';
-$port = 8889;
 
-$host = getenv('OPENSHIFT_MYSQL_DB_HOST');
-$port = getenv('OPENSHIFT_MYSQL_DB_PORT');
-$user = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-$pass = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$lokaal = false;
 
-print($host)."<br>";
-print($port)."<br>";
-print($user)."<br>";
-print($pass)."<br>";
+if($lokaal) {
+	$user = 'root';
+	$password = 'root';
+	$db = 'school';
+	$host = 'localhost';
+	$port = 8889;
+} else {
+	$user = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+	$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	$db = 'blogje';
+	$host = getenv('OPENSHIFT_MYSQL_DB_HOST');
+	$port = getenv('OPENSHIFT_MYSQL_DB_PORT');
+}
 
 $link = mysqli_init();
 $success = mysqli_real_connect(
