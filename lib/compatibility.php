6c6cee7f0c28c6b84daa97c267e4ddc4dad9208d
janjaap <janjaap@roc-dev.com>
2016-07-15 08:34:33 +0200

    versie checking

--- lib/compatibility.php
+++ lib/compatibility.php
@@ -1,5 +1,12 @@
 <?php
-print_r($_SERVER);
-$software = explode(" ", $_SERVER['SERVER_SOFTWARE']);
-print_r($software);
-echo phpversion();
+//print_r($_SERVER);
+//$software = explode(" ", $_SERVER['SERVER_SOFTWARE']);
+//print_r($software);
+//echo phpversion();
+
+if(PHP_MAJOR_VERSION < 7 && PHP_MINOR_VERSION < 5) {
+	echo "Versie: ".phpversion(). " is lager dan versie 5.5";
+} else {
+	echo "Versie: ".phpversion(). " is 5.5 of hoger";
+	
+}
